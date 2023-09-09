0c6406527832d9ff5cbbf1fb0456c6e8d03f39d1
yujinkim1 <yujin7621@me.com>
2023-09-09 17:23:20 +0900

    Update MapViewController

--- Doodle/Source/Presentation/Map/MapViewController.swift
+++ Doodle/Source/Presentation/Map/MapViewController.swift
@@ -1,72 +1,201 @@
 //
-//  ViewController.swift
-//  MapKitPlay
+//  MapViewController.swift
+//  Doodle
 //
-//  Created by 경원이 on 2022/01/21.
-//
+//  Created by ThePerfectMartini on 2022/09/04.
+//  Edited by yujinkim1 on 2023/09/09.
 
-import SnapKit
-import UIKit
-import MapKit
 import CoreLocation
+import MapKit
+import SnapKit
 import Then
+import UIKit
 
-
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
-    
-    let mapView = MapView()
-    
-    let spartaCoordinate = CLLocationCoordinate2D(latitude: 37.50262815810942, longitude: 127.04461259087897)
+class MapViewController: UIViewController {
     
     let locationManager = CLLocationManager()
     
-    
-    let width = 177.0
-    let height = 56.0
-    
-    
-    
+    let mapView = MapView()
     
     override func loadView() {
+        
         view = mapView
+        
     }
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        locationManager.requestWhenInUseAuthorization()
-        
-        mapView.map.setRegion(MKCoordinateRegion(center: spartaCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.001, longitudeDelta: 0.001)), animated: true)
-        
-        
-        mapView.map.delegate = self
-        locationManager.delegate = self
-        
-        addCustomPin()
+        setLocation()
         
         buttonAddAction()
+        
         buttonActions()
         
-        mapConfig()
-        
-        
-        
+        setMapViewConfigure()
         
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        sheetPresent()
+        
+        presentBottomSheet()
+        
     }
     
-    private func addCustomPin() {
-        let pin = MKPointAnnotation()
-        pin.coordinate = spartaCoordinate
-        pin.title = "테스트용 어노테이션 타이틀"
-        pin.subtitle = "테스트용 서브타이틀"
-        mapView.map.addAnnotation(pin)
+    private func setLocation() {
+        
+        locationManager.delegate = self
+        
+        locationManager.startUpdatingLocation()
+        
+        mapView.map.delegate = self
+        
     }
     
+    private func setMapViewConfigure() {
+        
+        mapView.map.isRotateEnabled = false
+        
+        mapView.map.isPitchEnabled = false
+        
+    }
+    
+    func goSetting() {
+        
+        let alert = UIAlertController(title: "위치권한 요청", message: "러닝 거리 기록을 위해 항상 위치 권한이 필요합니다.", preferredStyle: .alert)
+        let settingAction = UIAlertAction(title: "설정", style: .default) { action in
+            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
+            // 열 수 있는 url 이라면, 이동
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { UIAlertAction in
+            
+        }
+        
+        alert.addAction(settingAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func buttonActions() {
+        
+        mapView.myLocationButton.addTarget(self, action: #selector(findMyLocation), for: .touchUpInside)
+        
+    }
+    
+    func buttonAddAction() {
+        
+        mapView.addDoodleButton.addTarget(self, action: #selector(addButtonTapped), for: .touchUpInside)
+        
+    }
+    
+    func annotationClick() {
+        
+        self.dismiss(animated: false)
+        
+        let mapDetailViewController = MapDetailViewController()
+        
+        mapDetailViewController.closure = {self.presentBottomSheet()}
+        
+        mapDetailViewController.modalPresentationStyle = .pageSheet
+        
+        self.present(mapDetailViewController, animated: true)
+        
+    }
+    
+    func presentBottomSheet() {
+        let bottomViewController = bottomViewController()
+        bottomViewController.isModalInPresentation = true
+        if let sheet = bottomViewController.sheetPresentationController {
+            sheet.preferredCornerRadius = 30
+            sheet.detents = [
+                .custom(resolver: {
+                    0.15 * $0.maximumDetentValue
+                }),
+                .custom(resolver: {
+                    0.55 * $0.maximumDetentValue
+                }),
+                .large()]
+            sheet.largestUndimmedDetentIdentifier = .large
+        }
+        
+        self.present(bottomViewController, animated: true)
+
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
+        
+        annotationClick()
+        
+    }
+}
+
+//MARK: - Core Location Location Manager
+extension MapViewController: CLLocationManagerDelegate {
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        let authorizationStatus = manager.authorizationStatus
+        
+        switch authorizationStatus {
+                
+            case .notDetermined: manager.requestWhenInUseAuthorization()
+                
+            case .restricted, .denied: goSetting()
+                
+            case .authorizedAlways, .authorizedWhenInUse: manager.startUpdatingLocation()
+                
+            @unknown default: break
+
+        }
+        
+        if #available(iOS 14.0, *) {
+            
+            switch manager.accuracyAuthorization {
+                    
+                case .fullAccuracy: print("full")
+                    
+                case .reducedAccuracy: print("reduced")
+                    
+                @unknown default: print("unknown")
+                    
+                    
+            }
+            
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        
+        locationManagerDidChangeAuthorization(manager)
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if let location = locations.first {
+            
+            print("현재 나의 위치 정보: \(location)")
+            
+            manager.stopUpdatingLocation()
+            
+            let coordinateRegion = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 1000, longitudinalMeters: 1000)
+            
+            mapView.map.setRegion(coordinateRegion, animated: true)
+            
+        }
+        
+    }
+    
+}
+
+//MARK: - Map Kit Map View
+extension MapViewController: MKMapViewDelegate {
     
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard !annotation.isKind(of: MKUserLocation.self) else {
@@ -90,7 +219,7 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             imageView.snp.makeConstraints{
                 $0.edges.equalToSuperview().inset(UIEdgeInsets(top: 6.21, left: 6.21, bottom: 18.64, right: 6.21))
             }
-            var subtitleLabel = UILabel()
+            let subtitleLabel = UILabel()
             subtitleLabel.text = "이름이름"
             subtitleLabel.font = UIFont.systemFont(ofSize: 8)
             annotationView?.addSubview(subtitleLabel)
@@ -108,12 +237,6 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
             annotationView?.layer.shadowRadius = 4
             annotationView?.layer.shadowOffset = CGSize(width: 10, height: 10)
             annotationView?.layer.shadowPath = nil
-            
-//            annotationView?.canShowCallout = true
-            
-            
-            
-            
             let miniButton = UIButton(frame: CGRect(x: 0, y: 0, width: 20, height: 20))
             miniButton.setImage(UIImage(systemName: "person"), for: .normal)
             miniButton.tintColor = .blue
@@ -122,210 +245,62 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         } else {
             annotationView?.annotation = annotation
         }
-        
-        
         return annotationView
     }
     
-    
-    @objc func annotationTapped() {
-    }
-    
-    @objc func MoveLocation() {
-        
-        mapView.map.showsUserLocation = false
-        
-        mapView.map.userTrackingMode = .none
-        
-        mapView.map.setRegion(MKCoordinateRegion(center: spartaCoordinate, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
-    }
+}
+
+//MARK: - Custom Action
+extension MapViewController {
     
     @objc func findMyLocation() {
         
-        guard let currentLocation = locationManager.location else {
-            checkUserLocationServicesAuthorization()
-            return
-        }
+        guard let currentLocation = locationManager.location else { return }
         
         mapView.map.showsUserLocation = true
+        
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation.coordinate,
+                                                  latitudinalMeters: 1000,
+                                                  longitudinalMeters: 1000)
+        
+        mapView.map.setRegion(coordinateRegion, animated: true)
         
         mapView.map.setUserTrackingMode(.follow, animated: true)
         
     }
     
-    //권한 설정
-    
-    func checkCurrentLocationAuthorization(authorizationStatus: CLAuthorizationStatus) {
-        switch authorizationStatus {
-        case .notDetermined:
-            locationManager.requestWhenInUseAuthorization()
-            locationManager.startUpdatingLocation()
-        case .restricted:
-            print("restricted")
-            goSetting()
-        case .denied:
-            print("denided")
-            goSetting()
-        case .authorizedAlways:
-            print("always")
-        case .authorizedWhenInUse:
-            print("wheninuse")
-            locationManager.startUpdatingLocation()
-        @unknown default:
-            print("unknown")
-        }
-        if #available(iOS 14.0, *) {
-            let accuracyState = locationManager.accuracyAuthorization
-            switch accuracyState {
-            case .fullAccuracy:
-                print("full")
-            case .reducedAccuracy:
-                print("reduced")
-            @unknown default:
-                print("Unknown")
-            }
-        }
-    }
-    
-    func goSetting() {
+    @objc func changeLocation() {
         
-        let alert = UIAlertController(title: "위치권한 요청", message: "러닝 거리 기록을 위해 항상 위치 권한이 필요합니다.", preferredStyle: .alert)
-        let settingAction = UIAlertAction(title: "설정", style: .default) { action in
-            guard let url = URL(string: UIApplication.openSettingsURLString) else { return }
-            // 열 수 있는 url 이라면, 이동
-            if UIApplication.shared.canOpenURL(url) {
-                UIApplication.shared.open(url)
-            }
-        }
-        let cancelAction = UIAlertAction(title: "취소", style: .cancel) { UIAlertAction in
-            
-        }
+        mapView.map.showsUserLocation = false
         
-        alert.addAction(settingAction)
-        alert.addAction(cancelAction)
+        mapView.map.userTrackingMode = .none
+
+    }
+    
+    @objc func addButtonTapped() {
         
-        present(alert, animated: true, completion: nil)
-    }
-    
-    func checkUserLocationServicesAuthorization() {
-        let authorizationStatus: CLAuthorizationStatus
-        if #available(iOS 14, *) {
-            authorizationStatus = locationManager.authorizationStatus
-        } else {
-            authorizationStatus = CLLocationManager.authorizationStatus()
-        }
-        
-        if CLLocationManager.locationServicesEnabled() {
-            checkCurrentLocationAuthorization(authorizationStatus: authorizationStatus)
-        }
-    }
-    
-    
-    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
-        print(#function)
-        checkUserLocationServicesAuthorization()
-    }
-    
-    func buttonActions() {
-        mapView.myLocationButton.addTarget(self, action: #selector(findMyLocation), for: .touchUpInside)
-    }
-    
-    func buttonAddAction() {
-        mapView.addDoodleButton.addTarget(self, action: #selector(buttonAddClick), for: .touchUpInside)
-    }
-    
-    @objc func buttonAddClick(){
-        //        let CreateDoodleViewController = CreateDoodleViewController()
-        //        self.navigationController?.pushViewController(CreateDoodleViewController, animated: true)
-        self.dismiss(animated: false)
+        dismiss(animated: false)
         
         let createDoodleViewController = CreateDoodleViewController()
-//        createDoodleViewController.isModalInPresentation = true
+        
         createDoodleViewController.modalPresentationStyle = .fullScreen
+        
         self.present(createDoodleViewController, animated: true)
-
-        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(swipeDown))
-        swipeDown.direction = .down
-        createDoodleViewController.view.addGestureRecognizer(swipeDown)
+        
+        let swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swipeToDown))
+        
+        swipeGesture.direction = .down
+        
+        createDoodleViewController.view.addGestureRecognizer(swipeGesture)
         
     }
     
-    func annotationClick(){
-        //        let CreateDoodleViewController = CreateDoodleViewController()
-        //        self.navigationController?.pushViewController(CreateDoodleViewController, animated: true)
-        self.dismiss(animated: false)
+    @objc func swipeToDown(){
         
-        let mapDetailViewController = MapDetailViewController()
-        mapDetailViewController.closure = {self.sheetPresent()}
-        
-        //        mapDetailViewController.isModalInPresentation = true
-        mapDetailViewController.modalPresentationStyle = .pageSheet
-        self.present(mapDetailViewController, animated: true)
-        }
-
-    
-    @objc func swipeDown(){
         self.dismiss(animated: true)
-        sheetPresent()
-    }
         
-    func sheetPresent(){
-        let bottomViewController = bottomViewController()
-        bottomViewController.isModalInPresentation = true
-        if let sheet = bottomViewController.sheetPresentationController {
-            sheet.preferredCornerRadius = 30
-            sheet.detents = [
-                .custom(resolver: {
-                0.15 * $0.maximumDetentValue
-            }),
-                .custom(resolver: {
-                                 0.55 * $0.maximumDetentValue
-                             }),
-                .large()]
-            sheet.largestUndimmedDetentIdentifier = .large
-        }
-        self.present(bottomViewController, animated: true)
+        presentBottomSheet()
         
-        print("\n\n\n\n\n테스sda트\(self) - \n\n\n\n\n")
     }
 
-    
-    
-    private func mapConfig(){
-        mapView.map.isRotateEnabled = false
-        mapView.map.isPitchEnabled = false
-    }
-    
-    func mapView(_ mapView: MKMapView, didSelect annotation: MKAnnotation) {
-        annotationClick()
-        
-        print("\n TTAA \n")
-    }
 }
-
-
-
-
-//#if DEBUG
-//import SwiftUI
-//struct ViewControllerRepresentable: UIViewControllerRepresentable {
-//    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-//        // empty
-//    }
-//    @available(iOS 13.0.0, *)
-//    func makeUIViewController(context: Context) -> some UIViewController {
-//        MapViewController()
-//    }
-//}
-//@available(iOS 13.0, *)
-//struct SnapkitVCRepresentable_PreviewProvider: PreviewProvider {
-//    static var previews: some View {
-//        Group {
-//            ViewControllerRepresentable()
-//                .ignoresSafeArea()
-//                .previewDisplayName("preview")
-//                .previewDevice(PreviewDevice(rawValue: "iphone"))
-//        }
-//    }
-//} #endif
-
