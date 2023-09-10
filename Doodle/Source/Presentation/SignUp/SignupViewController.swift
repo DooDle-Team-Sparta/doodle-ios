3d5bcd5b1a85e1034208171259e46e0bc52fcc19
원성준 <tjdwns575859@gmail.com>
2023-09-10 16:08:13 +0900

    알럿기능 추가

--- Doodle/Source/Presentation/SignUp/SignupViewController.swift
+++ Doodle/Source/Presentation/SignUp/SignupViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-    
     private let emailPlaceholder = "이메일 주소"
     
     private let namePlaceholder = "이름"
@@ -269,165 +268,143 @@ class SignupViewController: UIViewController {
               let passwordCheck = passwordCheckTextField.text,
               !passwordCheck.isEmpty
         else {
-            
             print("빈칸을 채워주세요.")
-            
             return
-            
         }
-        
+
         // 비밀번호와 비밀번호확인이 일치할때만
         guard password == passwordCheck else {
-            
             print("비밀번호 일치하지않음.")
-            
             return
-            
         }
-        
+
         // 로딩화면
         showLoadingScreen()
-        
+
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            
             guard let self = self else { return }
-            
-            if var usersData = UserDefaults.standard.array(forKey: "users") as? [Data] {
-                
+
+            if let usersData = UserDefaults.standard.array(forKey: "users") as? [Data] {
                 for userData in usersData {
-                    
-                    if var user = try? JSONDecoder().decode(User.self, from: userData),
-                       
-                        user.email == email
+                    if let user = try? JSONDecoder().decode(User.self, from: userData),
+                       user.email == email
                     {
-                        
                         print("이미 사용자 있음.")
-                        
-                        // Add this line to remove the loading screen when the email is already registered.
+
                         DispatchQueue.main.async {
-                            
                             self.dismissLoadingScreen()
-                            
+                            self.registeredAlert()
                         }
-                        
                         return
-                        
                     }
-                    
                 }
-                
             }
             // user
             let user = User(email: email, name: name, password: password)
-            
+
             if var usersData = UserDefaults.standard.array(forKey: "users") as? [Data] {
-                
                 let userData = try! JSONEncoder().encode(user)
-                
                 usersData.append(userData)
-                
                 UserDefaults.standard.set(usersData, forKey: "users")
-                
+
+                //
                 self.emailTextField.text = ""
-                
                 self.nameTextField.text = ""
-                
                 self.passwordTextField.text = ""
-                
                 self.passwordCheckTextField.text = ""
-                
+
                 // 이전화면으로 이동(pop)
                 DispatchQueue.main.async { [weak self] in
-                    
                     guard let strongSelf = self else { return }
-                    
+
                     strongSelf.dismissLoadingScreen()
-                    
                     strongSelf.navigationController?.popViewController(animated: true)
                 }
-                
+
             } else {
-                
                 let userData = try! JSONEncoder().encode(user)
-                
                 UserDefaults.standard.set([userData], forKey: "users")
-                
             }
-            
         }
-        
+    }
+    
+    // 불투명 알럿창
+    func registeredShowToast(message: String) {
+        let toastLabel = UILabel(frame: CGRect(x: view.frame.size.width/2 - 120, y: 100, width: 240, height: 30))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.textAlignment = .center
+        toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+        toastLabel.text = message
+        toastLabel.alpha = 0.8
+        toastLabel.layer.cornerRadius = 10
+        toastLabel.clipsToBounds = true
+        view.addSubview(toastLabel)
+
+        UIView.animate(withDuration: 2.0, delay: 0.5, options: .curveEaseOut, animations: {
+            toastLabel.alpha = 0.0
+        }, completion: { isCompleted in
+            if isCompleted {
+                toastLabel.removeFromSuperview()
+            }
+        })
+    }
+
+    // 사용자 이미 등록되어있을때 나타나는 알럿창
+    func registeredAlert() {
+        registeredShowToast(message: "사용자가 이미 등록되어있습니다.")
     }
     
     // 텍스트필드 이외부분 터치했을때 키보드내려감
     func hideKeyboardTappedAround() {
-        
         let tap = UITapGestureRecognizer(target: self, action: #selector(SigninViewController.dismissKeyboard))
         
         tap.cancelsTouchesInView = false
         
         view.addGestureRecognizer(tap)
-        
     }
     
     @objc func dismissKeyboard() {
-        
         view.endEditing(true)
-        
     }
 }
 
 // MARK: - UITextFieldDelegate
 
 extension SignupViewController: UITextFieldDelegate {
-    
     // 텍스트필드 터치시 플레이스홀더 텍스트 사라짐
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
         if textField == emailTextField && textField.placeholder == emailPlaceholder {
-            
             textField.placeholder = ""
             
         } else if textField == nameTextField && textField.placeholder == namePlaceholder {
-            
             textField.placeholder = ""
             
         } else if textField == passwordTextField && textField.placeholder == passwordPlaceholder {
-            
             textField.placeholder = ""
             
         } else if textField == passwordCheckTextField && textField.placeholder == passwordCheckPlaceholder {
-            
             textField.placeholder = ""
-            
         }
-        
     }
     
     func textFieldDidEndEditing(_ textfield: UITextField) {
-        
         if textfield == emailTextField && (textfield.text?.isEmpty ?? true) {
-            
             textfield.placeholder = emailPlaceholder
             
         } else if textfield == nameTextField && (textfield.text?.isEmpty ?? true) {
-            
             textfield.placeholder = namePlaceholder
             
         } else if textfield == passwordTextField && (textfield.text?.isEmpty ?? true) {
-            
             textfield.placeholder = passwordPlaceholder
             
         } else if textfield == passwordCheckTextField && (textfield.text?.isEmpty ?? true) {
-            
             textfield.placeholder = passwordCheckPlaceholder
-            
         }
-        
     }
     
     // 키보드 엔터버튼 return으로 변경, return 버튼 누를시 키보드 내려감
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        
         textField.resignFirstResponder()
         
         dismiss(animated: true, completion: nil)
@@ -436,9 +413,7 @@ extension SignupViewController: UITextFieldDelegate {
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
         if textField == passwordTextField || textField == passwordCheckTextField, !string.isEmpty {
-            
             let allowedCharacters = CharacterSet.alphanumerics
             
             let characterSet = CharacterSet(charactersIn: string)
@@ -447,5 +422,16 @@ extension SignupViewController: UITextFieldDelegate {
         }
         
         return true
+    }
+    
+    // 비밀번호와 비밀번호 확인 일치할때 텍스트 색변화
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        if textField == passwordCheckTextField {
+            if passwordTextField.text == passwordCheckTextField.text {
+                passwordCheckTextField.backgroundColor = .white
+            } else {
+                passwordCheckTextField.backgroundColor = UIColor.systemGray4
+            }
+        }
     }
 }
