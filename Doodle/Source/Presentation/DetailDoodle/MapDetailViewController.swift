de0ac6296cb3ce1514c8e89ee3857f4551301a18
kiho <ehddns0528@gmail.com>
2023-09-10 16:40:33 +0900

    취소버튼 활성화

--- Doodle/Source/Presentation/DetailDoodle/MapDetailViewController.swift
+++ Doodle/Source/Presentation/DetailDoodle/MapDetailViewController.swift
@@ -10,11 +10,18 @@ class MapDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        mapDetailView.bottomCancelButton.addTarget(self, action: #selector(clickCancleBtn), for: .touchUpInside)
     }
     
     
     override func loadView() {
         view = mapDetailView
+    }
+    
+    @objc func clickCancleBtn(){
+        let BottomViewController = BottomViewController()
+        self.dismiss(animated: true)
     }
     
     
