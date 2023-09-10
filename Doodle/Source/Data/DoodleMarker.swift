9f2dab50e2be127dc81487ee4162cfc1807badaf
Kong <rhd9228@gmail.com>
2023-09-10 15:14:40 +0900

    어노테이션 눌렀을때 디테일뷰 이미지 전달, 위도 경도값 전달 구현 / 카카오맵 rest api 이용한 URL매니저 개발중

--- Doodle/Source/Data/DoodleMarker.swift
+++ Doodle/Source/Data/DoodleMarker.swift
@@ -15,4 +15,20 @@ class DoodleMarker: Codable {
     
     var location: [String: Double]?
     
+    var title: String?
+    
+    var likeCount: Int?
+}
+
+
+
+class singleton {
+    private init(){}
+    static var shared = singleton()
+    
+    var list = UserDefaults.standard.array(forKey: "DoodleList") ?? []
+    
+    static func saveDoodleList(){
+        UserDefaults.standard.set(singleton.shared.list, forKey: "DoodleList")
+    }
 }
---
+++ Doodle/Source/Extension/URLManager.swift
@@ -0,0 +1,63 @@
+
+import Foundation
+
+class URLManager {
+    let myKey = "405b8a57c501fcb6bbf75382f786f6af"
+    
+    static let shared = URLManager()
+    private init(){}
+    
+    
+    
+    func getJsonData(completion: @escaping (Result<Data, Error>) -> Void) {
+        func getJsonData(completion: @escaping (Result<Data, Error>) -> Void) {
+            let urlString = "https://dapi.kakao.com/v2/local/geo/coord2regioncode.json?x=127.1086228&y=37.4012191"
+            let urltoKorea = urlEncode.URLencode(urlString)
+            var request = URLRequest(url: urltoKorea)
+            request.setValue(myKey, forHTTPHeaderField: "Authorization")
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print("response 에러")
+                    completion(.failure(NetworkError.emptyResponse))
+                    return
+                }
+                guard let data = data else {
+                    print("data 에러")
+                    completion(.failure(NetworkError.emptyData))
+                    return
+                }
+                completion(.success(data))
+            }
+            task.resume()
+        }
+        
+        func getJsonDataDeal(id: String, completion: @escaping (Result<Data, Error>) -> Void) {
+            let urlString = "https://api.nexon.co.kr/fifaonline4/v1.0/users/\(id)/markets?tradetype=buy&offset=0&limit=100"
+            let urltoKorea = urlEncode.URLencode(urlString)
+            var request = URLRequest(url: urltoKorea)
+            request.setValue(myKey, forHTTPHeaderField: "Authorization")
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                if let error {
+                    print("에러")
+                    completion(.failure(NetworkError.emptyResponse))
+                    return
+                }
+                guard let data = data else {
+                    print("data 에러")
+                    completion(.failure(NetworkError.emptyResponse))
+                    return
+                }
+                completion(.success(data))
+            }
+            task.resume()
+        }
+        
+        
+    }
+    enum NetworkError: Error {
+        case emptyResponse
+        case emptyData
+    }
+}
